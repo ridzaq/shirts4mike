cc24dfb49128ec66d999f662e24288d5c0f1ffa2
Rizal <ridzaq@yahoo.com>
2014-06-15 18:53:06 +0800

    first push

---
+++ contact-process.php
@@ -0,0 +1,12 @@
+<pre><?php	
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	$email_body = "";
+	$email_body .= "Name: ".$name."\n";
+	$email_body .= "Email: ".$email."\n";
+	$email_body .= "Message: ".$message;
+	
+	header("Location: contact-thanks.php");
+	
+?></pre>
